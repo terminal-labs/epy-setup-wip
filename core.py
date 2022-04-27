3683238e37e1963b00aa5fb6506fb180c80312ac
Michael Verhulst <michael@terminallabs.com>
2022-04-27 13:40:24 -0500

    first commit

---
+++ core.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import time
+import json
+import yaml
+import shutil
+import hashlib
+import zipfile
+import base64
+import uuid
+from pathlib import Path
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="epythonsetup",
+    version='0.1',
+    install_requires=[],
+    packages=find_packages(),
+)
